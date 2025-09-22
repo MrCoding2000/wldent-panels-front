56e920e4be3cd604d8bed05152be39b28bdfabfa
MrCoding2000 <taha.softwareengineer2000@gmail.com>
2025-09-23 02:05:46 +0330

    implement translate lib

---
+++ libs/i18n/src/i18n.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {BehaviorSubject, catchError, forkJoin, Observable, of} from 'rxjs';
+
+export type Language = 'en' | 'fa';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class I18nService {
+  private currentLang = new BehaviorSubject<Language>('fa');
+  private translations: Record<string, any> = {};
+
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Set Language
+   * @param lang 'en' | 'fa'
+   * @param app
+   */
+  setLanguage(lang: Language, app: 'user-panel' | 'admin-panel') {
+    this.loadTranslations(lang, app);
+    if (this.currentLang.value === lang) return;
+    this.currentLang.next(lang);
+  }
+
+
+  /**
+   * Get Language
+   */
+  getLanguage(): Observable<Language> {
+    return this.currentLang.asObservable();
+  }
+
+  /**
+   * Translate
+   */
+  translate(key: string): string {
+    return this.translations[key] || key;
+  }
+
+  /**
+   * Load Translations
+   */
+  private loadTranslations(lang: Language, app: 'user-panel' | 'admin-panel') {
+    const sharedPath = `./assets/i18n/shared/${lang}.json`;
+    const appPath = `./assets/i18n/${app}/${lang}.json`;
+
+    forkJoin({
+      shared: this.http.get<Record<string, string>>(sharedPath).pipe(
+        catchError((err) => {
+          console.warn(`i18n: shared translation not found: ${sharedPath}`);
+          return of({});
+        })
+      ),
+      app: this.http.get<Record<string, string>>(appPath).pipe(
+        catchError((err) => {
+          console.warn(`i18n: app translation not found: ${appPath}`);
+          return of({});
+        })
+      ),
+    }).subscribe(({ shared, app }) => {
+      this.translations = { ...shared, ...app };
+    });
+  }
+}
---
+++ libs/services/src/lib/services/services.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-services',
+  imports: [],
+  templateUrl: './services.html',
+  styleUrl: './services.scss',
+})
+export class Services {}
