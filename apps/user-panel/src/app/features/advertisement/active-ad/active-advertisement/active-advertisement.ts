c28405abf16839bd8bb3c036d94483d8b871ea3d
MrCoding2000 <taha.softwareengineer2000@gmail.com>
2025-10-14 02:49:45 +0330

    continue active ad page

--- apps/user-panel/src/app/features/advertisement/active-ad/active-advertisement/active-advertisement.ts
+++ apps/user-panel/src/app/features/advertisement/active-ad/active-advertisement/active-advertisement.ts
@@ -1,15 +1,58 @@
-import { Component } from '@angular/core';
-import {FilterBar} from "@waldent-panels-front/ui";
+import {Component, OnInit} from '@angular/core';
+import {FilterBar, OptionalCard} from "@waldent-panels-front/ui";
+import {I18nService} from "@waldent-panels-front/translate";
+import {AdvertisementService} from "../../service/advertisement.service";
+import {OptionalCardsDataModel} from "@waldent-panels-front/models";
+import {DecimalPipe} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {ToggleSwitch} from "primeng/toggleswitch";
 
 @Component({
   selector: 'app-active-advertisement',
   imports: [
-    FilterBar
+    FilterBar,
+    OptionalCard,
+    DecimalPipe,
+    FormsModule,
+    ToggleSwitch
   ],
   standalone: true,
   templateUrl: './active-advertisement.html',
   styleUrl: './active-advertisement.scss'
 })
-export class ActiveAdvertisement {
+export class ActiveAdvertisement implements OnInit {
+  activeAdvertisementData!: OptionalCardsDataModel[];
+  checked: any;
 
+  constructor(public i18n: I18nService, private advertisementService: AdvertisementService) {
+  }
+
+  ngOnInit() {
+    this.getAdvertisementActiveCardsData();
+  }
+
+  /**
+   * TODO: AdvertisementRejectCardsData
+   */
+  getAdvertisementActiveCardsData() {
+    this.advertisementService.advertisementActiveCardsData().subscribe((data: OptionalCardsDataModel[]) => {
+      this.activeAdvertisementData = data;
+    })
+  }
+
+  /**
+   * TODO:Set Discount Request
+   * @param $event
+   */
+  onSetDiscountRequest($event: number) {
+
+  }
+
+  /**
+   * Edit Advertisement
+   * @param id
+   */
+  onEditAdvertisement(id: number) {
+
+  }
 }
---
+++ libs/ui/src/swiper-slider/swiper-slider.ts
@@ -0,0 +1,13 @@
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+
+@Component({
+  selector: 'lib-swiper-slider',
+  imports: [],
+  standalone: true,
+  templateUrl: './swiper-slider.html',
+  styleUrl: './swiper-slider.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class SwiperSlider {
+
+}
